fbea2f0f567ca7bfa2fcd34ae7b814cfaf72063f
Marcin Zdun <mzdun@midnightbits.com>
2017-04-02 20:54:10 +0200

    Added support for device and lights config

--- inc/shade/storage.h
+++ inc/shade/storage.h
@@ -1,11 +1,108 @@
 #pragma once
-#include <unordered_map>
+#include <array>
 #include <functional>
+#include <unordered_map>
+#include <vector>
 
 namespace shade {
+	struct light_state {
+		bool on;
+		int bri;
+		int hue;
+		int sat;
+		int ct;
+		std::array<double, 2> xy;
+		std::string mode;
+	};
+
+	struct group_state : light_state {
+		bool any;
+	};
+
+	struct info_base {
+		std::string id;
+		std::string name;
+		std::string type; // Group type or Light modelid
+	};
+
+	struct light_info : info_base {
+		light_state state;
+	};
+
+	struct group_info : info_base {
+		std::string klass;
+		group_state state;
+		std::vector<std::string> lights;
+	};
+
+#define MEM_EQ(name) (lhs.name == rhs.name)
+#define BASE_EQ(base) (static_cast<const base&>(lhs) == rhs)
+
+	inline bool operator == (light_state const& lhs, light_state const& rhs) {
+		return MEM_EQ(on)
+			&& MEM_EQ(bri)
+			&& MEM_EQ(hue)
+			&& MEM_EQ(sat)
+			&& MEM_EQ(ct)
+			&& MEM_EQ(xy[0])
+			&& MEM_EQ(xy[1])
+			&& MEM_EQ(mode);
+	}
+
+	inline bool operator != (light_state const& lhs, light_state const& rhs) {
+		return !(lhs == rhs);
+	}
+
+	inline bool operator == (group_state const& lhs, group_state const& rhs) {
+		return MEM_EQ(any)
+			&& BASE_EQ(light_state);
+	}
+
+	inline bool operator != (group_state const& lhs, group_state const& rhs) {
+		return !(lhs == rhs);
+	}
+
+	inline bool operator == (info_base const& lhs, info_base const& rhs) {
+		return MEM_EQ(id)
+			&& MEM_EQ(name)
+			&& MEM_EQ(type);
+	}
+
+	inline bool operator != (info_base const& lhs, info_base const& rhs) {
+		return !(lhs == rhs);
+	}
+
+	inline bool operator == (light_info const& lhs, light_info const& rhs) {
+		return MEM_EQ(state)
+			&& BASE_EQ(info_base);
+	}
+
+	inline bool operator != (light_info const& lhs, light_info const& rhs) {
+		return !(lhs == rhs);
+	}
+
+	inline bool operator == (group_info const& lhs, group_info const& rhs) {
+		return MEM_EQ(state)
+			&& MEM_EQ(klass)
+			&& MEM_EQ(lights)
+			&& BASE_EQ(info_base);
+	}
+
+	inline bool operator != (group_info const& lhs, group_info const& rhs) {
+		return !(lhs == rhs);
+	}
+
+#undef BASE_EQ
+#undef MEM_EQ
+
 	struct bridge_info {
 		std::string base;
 		std::string username;
+		std::string name;
+		std::string mac;
+		std::string modelid;
+		std::vector<light_info> lights;
+		std::vector<group_info> groups;
 	};
 
 	class store_at_exit;
@@ -22,6 +119,14 @@ namespace shade {
 	public:
 		void load();
 		auto const& bridges() const { return known_bridges; }
+		auto find(const std::string& id) const { return known_bridges.find(id); }
+		auto begin() const { return known_bridges.begin(); }
+		auto end() const { return known_bridges.end(); }
 		bool bridge_located(const std::string& id, const std::string& base);
+		void bridge_named(const std::string& id, const std::string& name, const std::string& mac, const std::string& modelid);
+		void bridge_connected(const std::string& id, const std::string& username);
+		void bridge_config(const std::string& id,
+			std::vector<light_info> lights,
+			std::vector<group_info> groups);
 	};
 }
--- src/storage.cc
+++ src/storage.cc
@@ -4,9 +4,44 @@
 #include <memory>
 
 namespace json {
+	JSON_STRUCT(shade::light_state) {
+		JSON_PROP(on);
+		JSON_PROP(bri);
+		JSON_PROP(hue);
+		JSON_PROP(sat);
+		JSON_PROP(ct);
+		JSON_PROP(xy);
+		JSON_PROP(mode);
+	};
+
+	JSON_STRUCT_(shade::group_state, shade::light_state) {
+		JSON_PROP(any);
+	};
+
+	JSON_STRUCT(shade::info_base) {
+		JSON_PROP(id);
+		JSON_PROP(name);
+		JSON_PROP(type);
+	};
+
+	JSON_STRUCT_(shade::light_info, shade::info_base) {
+		JSON_PROP(state);
+	};
+
+	JSON_STRUCT_(shade::group_info, shade::info_base) {
+		JSON_OPT_NAMED_PROP("class", klass);
+		JSON_PROP(state);
+		JSON_PROP(lights);
+	};
+
 	JSON_STRUCT(shade::bridge_info) {
 		JSON_PROP(base);
 		JSON_OPT_PROP(username);
+		JSON_OPT_PROP(name);
+		JSON_OPT_PROP(mac);
+		JSON_OPT_PROP(modelid);
+		JSON_OPT_PROP(lights);
+		JSON_OPT_PROP(groups);
 	}
 }
 
@@ -59,7 +94,7 @@ namespace shade {
 			return;
 
 		auto object = json::from_string(contents(in.get()));
-		if (object.get_type() != json::MAP)
+		if (!object.is<json::MAP>())
 			return;
 
 		if (!json::unpack(known_bridges, object))
@@ -68,12 +103,12 @@ namespace shade {
 
 	void storage::store()
 	{
+		auto text = json::pack(known_bridges)
+			.to_string(json::value::options::indented());
+
 		auto out = file::open(filename().c_str(), "w");
 		if (!out)
 			return;
-
-		auto text = json::pack(known_bridges)
-			.to_string(json::value::options::indented());
 		fprintf(out.get(), "%s\n", text.c_str());
 	}
 
@@ -93,4 +128,67 @@ namespace shade {
 		// if storage needs refreshing, this is a new/refreshed bridge
 		return !!needs_storing;
 	}
+
+	void storage::bridge_named(const std::string& id,
+		const std::string& name, const std::string& mac,
+		const std::string& modelid)
+	{
+		store_at_exit needs_storing{ this };
+		auto it = known_bridges.find(id);
+		if (it == known_bridges.end()) {
+			auto& bridge = known_bridges[id];
+			bridge.name = name;
+			bridge.mac = mac;
+			bridge.modelid = modelid;
+			needs_storing = true;
+		} else {
+			auto& bridge = it->second;
+			if (bridge.name != name
+				|| bridge.mac != mac
+				|| bridge.modelid != modelid) {
+				bridge.name = name;
+				bridge.mac = mac;
+				bridge.modelid = modelid;
+				needs_storing = true;
+			}
+		}
+	}
+
+	void storage::bridge_connected(const std::string& id, const std::string& username)
+	{
+		store_at_exit needs_storing{ this };
+		auto it = known_bridges.find(id);
+		if (it == known_bridges.end()) {
+			known_bridges[id].username = username;
+			needs_storing = true;
+		} else {
+			if (it->second.username != username) {
+				it->second.username = username;
+				needs_storing = true;
+			}
+		}
+	}
+
+	void storage::bridge_config(const std::string& id,
+		std::vector<light_info> lights,
+		std::vector<group_info> groups)
+	{
+		store_at_exit needs_storing{ this };
+		auto it = known_bridges.find(id);
+		if (it == known_bridges.end()) {
+			auto& bridge = known_bridges[id];
+			bridge.lights = std::move(lights);
+			bridge.groups = std::move(groups);
+			needs_storing = true;
+		} else {
+			auto& bridge = it->second;
+			if (bridge.lights != lights
+				|| bridge.groups != groups) {
+				bridge.lights = std::move(lights);
+				bridge.groups = std::move(groups);
+				needs_storing = true;
+			}
+		}
+	}
+
 }
